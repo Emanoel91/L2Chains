761562c341b87d349c82368cca1755cdaf130ed0
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-21 03:03:07 +0330

    Update L2Chains.py

--- L2Chains.py
+++ L2Chains.py
@@ -17,8 +17,8 @@ st.title('Arbitrum vs. Optimism')
 # Content
 c1, c2 = st.columns(2)
 
-c1.image(Image.open('Images/Arbitrum.png'))
-c2.image(Image.open('Images/op.png'))
+c1.image(Image.open('Images/logo-rb.JPG'))
+c2.image(Image.open('Images/logo-op.JPG'))
 
 st.subheader('📃 Introduction')
 
