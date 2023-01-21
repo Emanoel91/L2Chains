f4bfbb808209a6f6a96669979c47fa60bbad1518
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-21 03:37:20 +0330

    Update L2Chains.py

--- L2Chains.py
+++ L2Chains.py
@@ -95,71 +95,20 @@ with subtab_ALICE:
      with c2:
             df = ALICE_Price_ATH
             st.metric(label='Range of Price Change', value=df['RoPC'])
-     c1, c2 = st.columns(2)
-     with c1:             
-             df = ALICE_Price
-             fig = px.bar(df, x='DATE', y='RoPC', title='Range of Price Changes', log_y=False)
-             fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='$USD')
-             st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
-     with c2:
-            df = ALICE_Price_Metric
-            fig = px.line(df, x='Day', y='Price', color='TYPE', title='Price per Day', log_y=False)
-            fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='$USD', xaxis={'categoryorder':'total ascending'})
-            st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)	
+
 
 with subtab_AXS:
-  c1, c2 = st.columns(2)
-  with c1:
-        df = ALICE_Price
-        fig = px.bar(df, x='DATE', y='RoPC', title='Range of Price Changes', log_y=False)
-        fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='$USD')
-        st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
-	
-	
-  with c2:
-        df = AXS_Price_Metric
-        fig = px.line(df, x='Day', y='Price', color='TYPE', title='Price per Day', log_y=False)
-        fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='$USD', xaxis={'categoryorder':'total ascending'})
-        st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+
 	   	
 with subtab_ENJ:
-     c1, c2 = st.columns(2)
-     with c1:
-             df = ALICE_Price
-             fig = px.bar(df, x='DATE', y='RoPC', title='Range of Price Changes', log_y=False)
-             fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='$USD')
-             st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
-     with c2:
-            df = ENJ_Price_Metric
-            fig = px.line(df, x='Day', y='Price', color='TYPE', title='Price per Day', log_y=False)
-            fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='$USD', xaxis={'categoryorder':'total ascending'})
-            st.plotly_chart(fig, use_container_width=True, theme=theme_plotly) 
+
          
 with subtab_MANA:
-     c1, c2 = st.columns(2)
-     with c1:
-             df = ALICE_Price
-             fig = px.bar(df, x='DATE', y='RoPC', title='Range of Price Changes', log_y=False)
-             fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='$USD')
-             st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
-     with c2:
-            df = MANA_Price_Metric
-            fig = px.line(df, x='Day', y='Price', color='TYPE', title='Price per Day', log_y=False)
-            fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='$USD', xaxis={'categoryorder':'total ascending'})
-            st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+
                
 with subtab_SAND:
-     c1, c2 = st.columns(2)
-     with c1:
-             df = ALICE_Price
-             fig = px.bar(df, x='DATE', y='RoPC', title='Range of Price Changes', log_y=False)
-             fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='$USD')
-             st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
-     with c2:
-            df = SAND_Price_Metric
-            fig = px.line(df, x='Day', y='Price', color='TYPE', title='Price per Day', log_y=False)
-            fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='$USD', xaxis={'categoryorder':'total ascending'})
-            st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+
+
              
 #-----------------------------------------------------------------------------------------------------------
 c1, c2, c3 = st.columns(3)
