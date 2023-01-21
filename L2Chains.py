12f422f9f13fc763285f25704fece19583459525
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-21 04:06:26 +0330

    Update L2Chains.py

--- L2Chains.py
+++ L2Chains.py
@@ -74,7 +74,18 @@ with subtab_Daily:
 		
             fig = px.bar(df, x='Day', y='Total TX Fee', color='L2 Chain', title='Total Transaction Fees', log_y=False)
             fig.update_layout(showlegend=True, xaxis_title=None, legend_title='L2 Chain', yaxis_title='$ETH', xaxis={'categoryorder':'total ascending'})
+            st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+
+            fig = px.bar(df, x='Day', y='Total TX Value', color='L2 Chain', title='Total Transactions Value', log_y=False)
+            fig.update_layout(showlegend=True, xaxis_title=None, legend_title='L2 Chain', yaxis_title='$ETH', xaxis={'categoryorder':'total ascending'})
             st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)		
+	
+	
+            fig = px.line(df, x='Day', y='Average TX Value', color='L2 Chain', title='Average Transactions Value', log_y=False)
+            fig.update_layout(showlegend=True, xaxis_title=None, legend_title='L2 Chain', yaxis_title='$ETH', xaxis={'categoryorder':'total ascending'})
+            st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)		
+	
+	
 
 with subtab_Weekly:
      c1, c2 = st.columns(2)
