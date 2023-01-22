a79cd9cb9c6a50ae6424e1714bb03960e6c1c179
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-22 14:00:36 +0330

    Update L2Chains.py

--- L2Chains.py
+++ L2Chains.py
@@ -197,19 +197,19 @@ with subtab_Daily:
                 fig.update_layout(showlegend=True, xaxis_title=None, legend_title='STATUS', yaxis_title='TXs Count', xaxis={'categoryorder':'total ascending'})
                 st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)	
 	   	
-              df = Top_20_Events_Based_on_TXs_Count_Arbitrum
-              c1, c2 = st.columns(2)
-             
-              with c1:
-	            fig = px.bar(df, x='Event', y='TX Count', color='Event', title='🔵Arbitrum: Top 20 Events Based TXs Count', log_y=False)
-                    fig.update_layout(showlegend=False, xaxis_title=None, legend_title='', yaxis_title='TXs Count', xaxis={'categoryorder':'total ascending'})
-                    st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+            df = Top_20_Events_Based_on_TXs_Count_Arbitrum
+            c1, c2 = st.columns(2)
+            
+            with c1:
+	        fig = px.bar(df, x='Event', y='TX Count', color='Event', title='🔵Arbitrum: Top 20 Events Based TXs Count', log_y=False)
+                fig.update_layout(showlegend=False, xaxis_title=None, legend_title='', yaxis_title='TXs Count', xaxis={'categoryorder':'total ascending'})
+                st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
 		
-              df = Top_20_Events_Based_on_TXs_Count_Optimism
-              with c2:
-	            fig = px.bar(df, x='Event', y='TX Count', color='Event', title='🔴Optimism: Top 20 Events Based TXs Count', log_y=False)
-                    fig.update_layout(showlegend=False, xaxis_title=None, legend_title='', yaxis_title='TXs Count', xaxis={'categoryorder':'total ascending'})
-                    st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)		
+            df = Top_20_Events_Based_on_TXs_Count_Optimism
+            with c2:
+	        fig = px.bar(df, x='Event', y='TX Count', color='Event', title='🔴Optimism: Top 20 Events Based TXs Count', log_y=False)
+                fig.update_layout(showlegend=False, xaxis_title=None, legend_title='', yaxis_title='TXs Count', xaxis={'categoryorder':'total ascending'})
+                st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)		
 		
 with subtab_Daily:
 
